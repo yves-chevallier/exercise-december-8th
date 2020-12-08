9b01bafd1d45ed7ba17b475142fb9a1e0ee30041
Yves Chevallier <yves-chevallier@heig-vd.ch>
2020-12-08 13:50:03 +0100

    Identified max and min

--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 - [x] Présentation de l'exercice...
 - [x] Déclarer référentiel GitHub exercice (4min)
 - [x] Déclarer un tableau de 128 float 32-bits (5min) -> 13h30
-- [ ] Remplir ce tableau avec des valeurs aléatoires 0..255 (10min)
-- [ ] Identifier la valeur max. du tableau et la valeur min. puis afficher (5min)
+- [x] Remplir ce tableau avec des valeurs aléatoires 0..255 (10min)
+- [x] Identifier la valeur max. du tableau et la valeur min. puis afficher (5min)
 - [ ] Normaliser ce tableau à 1.0 (10min)
 - [ ] Afficher le tableau sur la sortie standard (5min)
 
--- exercise.c
+++ exercise.c
@@ -1,5 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <float.h>
+
+void sizeof_example() {
+    int i[10];
+    printf("sizeof(i)=%ld\n", sizeof(i));
+    printf("sizeof(i[0])=%ld\n", sizeof(i[0]));
+
+    printf("sizeof(long double)=%ld\n", sizeof(long double));
+    printf("sizeof(double)=%ld\n", sizeof(double));
+    printf("sizeof(float)=%ld\n", sizeof(float));
+    printf("sizeof(int)=%ld\n", sizeof(int));
+    printf("sizeof(short)=%ld\n", sizeof(short));
+    printf("sizeof(char)=%ld\n", sizeof(char));
+}
 
 int main() {
     float array[128] = {0};
+    const int size = sizeof(array) / sizeof(array[0]);
+
+    float max = FLT_MIN, min = FLT_MAX;
+
+    for (int i = 0; i < size; i++) {
+        array[i] = rand() % 256;
+        if (array[i] > max) max = array[i];
+        if (array[i] < min) min = array[i];
+        printf("array[%d] = %f\n", i, array[i]);
+    }
+
+    printf("max = %f\n", max);
+    printf("min = %f\n", min);
+
+
 }
